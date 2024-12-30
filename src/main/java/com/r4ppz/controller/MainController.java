dc626bf2e2c79a06d7bdcc452af9d27cf82290ae
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-30 21:36:38 +0800

    Add chapter navigation using TreeView in MainController for improved content organization

--- src/main/java/com/r4ppz/controller/MainController.java
+++ src/main/java/com/r4ppz/controller/MainController.java
@@ -1,31 +1,61 @@
 package com.r4ppz.controller;
 
+import java.util.Collections;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class MainController {
     @FXML
     private TextFlow contentTextFlow;
+    @FXML
+    private TreeView<String> chaptersTreeView;
 
     @FXML
     private void initialize() {
         loadTextToTextFlow();
+        loadChapterToTreeView();
     }
 
     private void loadTextToTextFlow() {
-        Text header = new Text("What is Java?\n");
-        header.getStyleClass().add("header-one");
+        Text whatIsJavaDeader = new Text("What is Java?\n");
+        whatIsJavaDeader.getStyleClass().add("header-one");
 
-        Text description = new Text(
-            "Java is a popular programming language that was first introduced by Sun Microsystems in 1995. It" +
-            "is widely used across the world for creating all kinds of applications, from mobile apps to web" +
-            "applications, desktop programs, and even large-scale enterprise systems. Java is loved for its" +
-            "versatility, reliability, and platform independence. Let\'s break down what makes Java special:"
-        );
-        description.getStyleClass().add("body-paragraph");
+        Text javaDescription = new Text(
+                "Java is a popular programming language that was first introduced by Sun Microsystems in 1995. It" +
+                        " is widely used across the world for creating all kinds of applications, from mobile apps to web"
+                        +
+                        "applications, desktop programs, and even large-scale enterprise systems. Java is loved for its"
+                        +
+                        "versatility, reliability, and platform independence. Let\'s break down what makes Java special:\n\n");
+        javaDescription.getStyleClass().add("body-paragraph");
 
+        Text keyFeaturesOfJava = new Text("Key Features of Java\n");
+        keyFeaturesOfJava.getStyleClass().add("header-two");
 
-        contentTextFlow.getChildren().addAll(header, description);
+        contentTextFlow.getChildren().addAll(whatIsJavaDeader, javaDescription, keyFeaturesOfJava);
+    }
+
+    public void loadChapterToTreeView() {
+        TreeItem<String> rootItem = new  TreeItem<>("Chapters");
+        rootItem.setExpanded(true);
+        
+        TreeItem<String> chapter1 = new TreeItem<>("Chapter 1");
+        TreeItem<String> chapter2 = new TreeItem<>("Chapter 2");
+        TreeItem<String> chapter3 = new TreeItem<>("Chapter 3");
+        TreeItem<String> chapter4 = new TreeItem<>("Chapter 4");
+        
+        TreeItem<String> topic1 = new TreeItem<>("What is Java");
+        TreeItem<String> topic2 = new TreeItem<>("History of Java");
+        TreeItem<String> topic3 = new TreeItem<>("Key Features of Java");
+
+        // Add topics to Chapter 1
+        Collections.addAll(chapter1.getChildren(), topic1, topic2, topic3);
+
+        rootItem.getChildren().addAll(chapter1, chapter2, chapter3, chapter4);
+        chaptersTreeView.setRoot(rootItem);
     }
 }
